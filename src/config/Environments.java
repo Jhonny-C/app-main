d80180a79bc719f1e0e4e77df9727ed3f6e8e560
Jhonny-C <jtacuri1281@gmail.com>
2023-12-05 18:02:57 -0400

    primer commit

---
+++ src/config/Environments.java
@@ -0,0 +1,7 @@
+package config;
+
+public class Environments {
+    public static String lag = "es";
+    public static String pathFile = "C:\\Users\\jcond\\Downloads\\app-main\\App\\data\\user.txt";
+    public static String pathFileProducts = "C:\\Users\\jcond\\Downloads\\app-main\\App\\data\\product.txt";
+}
